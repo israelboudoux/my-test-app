17c0170cc538b26c2b6a620173f2a08f47a15e1a
israelboudoux <30274053+israelboudoux@users.noreply.github.com>
2019-12-13 19:25:47 +0000

    Removing license text

--- src/main/java/com/triggerise/util/CsvUtils.java
+++ src/main/java/com/triggerise/util/CsvUtils.java
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) 2019 WIT Software <info@wit-software.com>. All rights reserved.
- * <p>
- * WIT Software Confidential and Proprietary information. It is strictly forbidden for 3rd parties to modify, decompile,
- * disassemble, defeat, disable or circumvent any protection mechanism; to sell, license, lease, rent, redistribute or
- * make accessible to any third party, whether for profit or without charge.
- */
 package com.triggerise.util;
 
 import java.io.File;
